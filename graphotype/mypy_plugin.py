de9a6b74d16f7946097fd8b487209a1795b72a2a
Ben Kuhn <ben.s.kuhn@gmail.com>
2019-02-05 23:28:37 +0000

    Switch to dynamic class def

--- graphotype/mypy_plugin.py
+++ graphotype/mypy_plugin.py
@@ -1,13 +1,14 @@
 from typing import Optional, Callable
 
-from mypy.plugin import Plugin, FunctionContext, MethodContext
+from mypy.plugin import Plugin, FunctionContext, DynamicClassDefContext
 from mypy.types import Type, LiteralType
+from mypy.nodes import TypeInfo, SymbolTable, ClassDef, Block
 
 class CustomPlugin(Plugin):
     def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], Type]]:
         if fullname == 'graphotype.Schema':
             return schema_callback
-    def get_method_hook(self, fullname: str) -> Optional[Callable[[MethodContext], Type]]:
+    def get_dynamic_class_hook(self, fullname: str) -> Optional[Callable[[DynamicClassDefContext], TypeInfo]]:
         if fullname == 'graphotype.Schema.query':
             return query_callback
 
@@ -18,12 +19,19 @@ def schema_callback(ctx: FunctionContext) -> Type:
         args=[LiteralType(fname, ctx.arg_types[0][0])]
     )
 
-def query_callback(ctx: MethodContext) -> Type:
+def query_callback(ctx: DynamicClassDefContext) -> TypeInfo:
     # todo be defensive--ctx.type is Schema[Literal[fname]]
     fname = ctx.type.args[0].value
     query = ctx.args[0][0].value
     print(fname, query)
-    return ctx.default_return_type
+    return TypeInfo(
+        names=SymbolTable(),
+        defn=ClassDef(
+            'Query',
+            defs=Block([])
+        ),
+        module_name='example'
+    )
 
 def plugin(version: str):
     # ignore version argument if the plugin works with all mypy versions.
---
+++ tests/test_type_gen.py
@@ -0,0 +1,3 @@
+from graphotype.mypy_plugin import query_type_for_schema
+
+print(query_type_for_schema)
